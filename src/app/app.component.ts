10a3c816ddcab1e7821c7f6e3d702b2f062de73b
Brin29 <breinerstivenparracortes50@gmail.com>
2024-11-28 10:47:12 -0500

    creacion de los routers en el header y app.route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink , HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
