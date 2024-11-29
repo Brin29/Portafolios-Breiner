cb4ccf65858dbd9917cdeb0a0f8321c9401a7b92
Brin29 <breinerstivenparracortes50@gmail.com>
2024-11-28 19:55:18 -0500

    styles with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "media",
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      "purple-dark": "#59517a",
+      "purple-semi-dark": "#353540",
+      "purple-semi-clean": "#b7addb",
+      "purple-very-clean": "#eddbff",
+      "white": "#e0e0e0",
+      "dark": "#1e1f29"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
