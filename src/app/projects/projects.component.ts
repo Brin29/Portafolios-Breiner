e2cd6ef597064d6fe5244c8c588d42cea24b3aaf
Brin29 <breinerstivenparracortes50@gmail.com>
2025-02-06 23:21:30 -0500

    sa

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Projects } from '../projects';
 @Component({
   selector: 'app-projects',
   imports: [],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit{
+
+  projects: Projects[] = [];
+
+  ngOnInit(): void {      
+    this.projects = [
+      {
+        id: 1,
+        name: "Authorization with JWT",
+        description: "lorem",
+        image: "#/",
+        frontEnd: ["", ""],
+        backEnd: ["", ""],
+        frontUrl: "sa",
+        backUrl: "sa",
+      },
+      {
+        id: 2,
+        name: "Shorten Url System",
+        description: "lorem",
+        image: "#/",
+        frontEnd: ["", ""],
+        backEnd: ["", ""],
+        frontUrl: "sa",
+        backUrl: "sa",
+      },
+      {
+        id: 3,
+        name: "Chat with WebSocket",
+        description: "lorem",
+        image: "#/",
+        frontEnd: ["", ""],
+        backEnd: ["", ""],
+        frontUrl: "sa",
+        backUrl: "sa",
+      },
+
+    ]
+  }
 
 }
